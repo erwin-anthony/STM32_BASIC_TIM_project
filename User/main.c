3653eeed0d5d4a249b5d10c807f975be25a22289
erwin_anthony <anthony.20140617@gmail.com>
2024-04-17 12:38:27 +0800

    基本模块

---
+++ User/main.c
@@ -0,0 +1,28 @@
+#include "stm32f10x.h"                  // Device header
+#include "bsp_led.h"
+
+
+
+//定义一个简单的延时函数
+void Delay(uint32_t count){
+		
+	for(;count != 0;count--){
+	
+	}
+	
+}
+
+
+int main(void){
+	
+	
+
+
+	
+	while(1){
+		
+
+		
+	}
+	
+}
